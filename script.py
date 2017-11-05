182259a203b2785310f1f5bc89b6ec53566aa05f
kaliqwang <kaliqwang@yahoo.com>
2017-11-05 13:16:26 -0500

    script update

--- script.py
+++ script.py
@@ -1,16 +1,15 @@
-import os, sys
+import os, sys, django
 
 proj_path = "/home/fijisurvivor/Survivor"  # TODO: hardcoded
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Survivor.settings")
-sys.path.append(proj_path)
-
-os.chdir(proj_path)
-
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+django.setup()
+# sys.path.append(proj_path)
+# os.chdir(proj_path)
+#
+# from django.core.wsgi import get_wsgi_application
+# application = get_wsgi_application()
 
 from django.core import management
-
 management.call_command('check_game_start')
 management.call_command('quota_check')
